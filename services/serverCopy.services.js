af0902fbf0393e02735b021cf1a3aa2344a0a9ba
shofikul <179398ssshofik@gmail.com>
2024-08-26 11:27:41 +0600

    fix: update price list

--- services/serverCopy.services.js
+++ services/serverCopy.services.js
@@ -2,34 +2,39 @@ const ServerCopy = require("../models/ServerCopy");
 
 // create a ServerCopy
 exports.createNewServerCopyService = async (data) => {
-    const result = await ServerCopy.create(data);
-    return result;
-}
+  const result = await ServerCopy.create(data);
+  return result;
+};
 
 // get all ServerCopy
 exports.getAllServerCopyService = async () => {
-    const result = await ServerCopy.find({});
-    return result;
-}
+  const result = await ServerCopy.find({});
+  return result;
+};
 
 // get a ServerCopy
 exports.getAServerCopyService = async (_id) => {
-    const result = await ServerCopy.findOne({ _id });
-    return result;
-}
+  const result = await ServerCopy.findOne({ _id });
+  return result;
+};
 exports.getAServerCopyDataByEmailService = async (email) => {
-    const result = await ServerCopy.find({ email });
-    return result;
-}
+  const result = await ServerCopy.find({ email });
+  return result;
+};
 
 // update a ServerCopy
 exports.updateAServerCopyService = async ({ _id, data }) => {
-    const result = await ServerCopy.updateOne({ _id }, { $set: { ...data } }, { upsert: true });
-    return result;
-}
+  console.log("_id and data", _id, data);
+  const result = await ServerCopy.updateOne(
+    { _id },
+    { $set: { ...data } },
+    { upsert: true }
+  );
+  return result;
+};
 
 // delete a ServerCopy
 exports.deleteAServerCopyService = async (_id) => {
-    const result = await ServerCopy.deleteOne({ _id });
-    return result;
-}
+  const result = await ServerCopy.deleteOne({ _id });
+  return result;
+};
